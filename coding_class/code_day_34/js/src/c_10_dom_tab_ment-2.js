07ff115f8f113d38d17ddf4523b5a59b442c9556
b_gyuwan <gyuwanc@gmail.com>
2021-12-02 13:13:09 +0900

    deleted folder

--- coding_class/code_day_34/js/src/c_10_dom_tab_ment-2.js
+++
@@ -1,9 +0,0 @@
-//c_10_dom_tab_ment-2.js
-
-
-var innerCode = '<a href="#" data-id><h4 class="event_title"></h4><p class="event_narration"></p><dl class="date"><dt class="blind">기간</dt><dd>2022.01.05 - 2022.02-15</dd></dl><dl class="event_check success"><dt></dt><dd></dd></dl></a>';
-
-//innerHTMl로  HTMl에 삽입.
-
-//------------------------------------------------------
-//탭메뉴 내용에 필요한 코드 형식 삽입하기
