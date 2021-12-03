4f15e94fc4a1c456301d51f3667f62aefbee9174
b_gyuwan <gyuwanc@gmail.com>
2021-12-03 18:30:02 +0900

    [add]modal && tab menu JS

--- coding_class/code_day_34/js/src/c_10_dom_modal.js
+++ coding_class/code_day_34/js/src/c_10_dom_modal.js
@@ -1,29 +1,53 @@
-//c_10_dom_modal.js
+// c_10_dom_modal.js
 
-//++++++++++++++++++++++++
-//시나리오:
-//1.li카드를 선택시 모달창이 나타나게 만들기
-//2.카드 클릭시 모달창을 띄울 요소를 생성(?)
-//3.모달창이 서서히 나타나게 하고 나타나면, 
-//4.해당하는 모달창의 닫기 버튼에 자동으로 focus처리
-//5.모달창의 닫기버튼을 클릭하면, 기존에있던 li카드의 위치로 돌아가서 해당카드에 focus처리
+// ++++++++++++++++++++++++++++
+// 시나리오:
+// * li카드를 선택시 모달창이 나타나게 만들기
+// 1. 카드 클릭시 모달창을 띄울 요소를 생성(???) // 추후 처리
+// 2. 모달창이 서서히 나타나게 하고(fade) // 일단 class로 대체
+// 3. 모달창이 나타나면, 해당하는 모달창의 닫기 버튼에 자동으로 focus 처리
+// 4. 모달창의 닫기버튼을 클릭하면, 기존에 있던 li카드의 위치로 돌아가서 해당 카드에 focus 처리
+// ++++++++++++++++++++++++++++
 
-//++++++++++++++++++++++++
+// ============================
+// 변수
+var elEventBox       = document.querySelector('#eventBox');
+var elContentInner   = elEventBox.querySelector('.content_inner');
+var elYearPart       = elContentInner.querySelector('.year_part.on');
+var elYearLi         = elYearPart.querySelectorAll('li a');
+var elYearLiSelector = [].slice.call(elYearLi);
+// console.log( elYearLiSelector );
 
-//========================
-//변수
+var elModal          = elEventBox.querySelector('.event_modal');
+var elModalCloseBtn  = elModal.querySelector('.modal_close button');
 
-//========================
-//기능
+var OPTION_CHECK     = 'on';
+var cardIndex        = 0; 
+// ============================
+// 기능
+// ============================
+// 함수
+// ============================
+// 이벤트
+// 모달 나타나게하기, 닫기버튼에 focus
+elYearLiSelector.forEach(function(element, index){
+  element.addEventListener('click', function(e){
+    e.preventDefault();
+    // this.getAttribute('data-href') // 차후 관련 주소를 통해 필요한 data를 처리.... 
+    cardIndex = index;   
+    elModal.classList.add(OPTION_CHECK);
+    elModalCloseBtn.focus();
+  });
+}); 
 
-//========================
-//함수
+// 모달 사라지게하기, 클릭했던 li위치로 돌아가기
+elModalCloseBtn.addEventListener('click', function(e){
+  e.preventDefault();
+  elModal.classList.remove(OPTION_CHECK);
+  elYearLiSelector[cardIndex].focus();
+});
 
-//========================
-//이벤트
-
-//========================
-
-//++++++++++++++++++++++++
-//첨부
-//++++++++++++++++++++++++
+// ============================
+// +++++++++++++++++++++++++++
+// 첨부 : 
+// +++++++++++++++++++++++++++
--- coding_class/code_day_34/js/src/c_11_dom_insert_data.js
+++ coding_class/code_day_34/js/src/c_11_dom_insert_data.js
@@ -1,134 +1,139 @@
-//c_11_dom_insert_data.js
-
-//별도 자료 ------------------------------------------------
+// c_11_dom_insert_data.js
+// 별도 자료 ----------------------------
 var data = [
-{ 
-  title:'title'  , 
-  contents: 'ab', lin:'http://naver.com',
-  bgImg:'../multi/img/bottle.jpg',
-  bgNarr:'병 이미지에 대한 설명'
-}, //data[0].title
+  {
+    title:'title'      , 
+    contents:'Lorem ipsum dolor sit amet consectetur adipisicing elit.',
+    link:'http://naver.com', 
+    bgImg:'../multi/img/bottle.jpg',
+    bgNarr:'병이미지에 대한 간략한 설명'
+  }, 
 
-{ 
-  title:'spring'  , 
-  contents: 'bc', lin:'http://naver.com',
-  bgImg:'../multi/img/cookie.jpg',
-  bgNarr:'쿠키 이미지에 대한 설명'
-}, //data[1].title
+  {
+    title:'spring'     , 
+    contents:'Dolores laborum expedita eum maxime dolore porro ipsam.' ,
+    link:'http://csswinner.com' ,
+    bgImg:'../multi/img/cookie.jpg',
+    bgNarr:'cookie 이미지에 설명 작성'
+  },
 
-{ 
-  title:'javascript'  , 
-  contents: 'cd', lin:'http://naver.com'
-}, //data[2].title
+  {
+    title:'javascript' , 
+    contents:'Quod, impedit?' ,
+    link:'http://gdweb.co.kr' 
+  },
 
-{
-  title:'winter'  , 
-  contents: 'de', lin:'http://naver.com',
-  bgImg:'../multi/img/bottle.jpg',
-  bgNarr:'병 이미지에 대한 설명'
-}, //data[3].title
+  {
+    title:'winter'     , 
+    contents:'Rem enim tempore maxime voluptas facilis id ad commodi quis.' ,
+    link:'http://naver.net',
+    bgImg:'../multi/img/bottle.jpg',
+    bgNarr:'병이미지에 대한 간략한 설명'
+  },
+  
+  {
+    title:'summer'     , 
+    contents:'Lorem, ipsum dolor.' ,
+    link:'http://google.com',
+    bgImg:'../multi/img/juice.jpg',
+    bgNarr:'juice image 설명'
+  }, 
 
-{
-  title:'summer'  , 
-  contents: 'ef', lin:'http://naver.com',
-  bgImh:'../multi/img/juice.jpg',
-  bgNarr:'주스 이미지에 대한 설명'
-}, //data[4].title
-
-{ 
-  title:'banana'  , 
-  contents: 'fg'
-}, //data[5].title
-
-{
-  title:'group'  , 
-  contents: 'gh',
-  bgImh:'../multi/img/bottle.jpg',
-  bgNarr:'병 이미지에 대한 설명'
-}//data[6].title
+  {
+    title:'banana'     , 
+    contents:'Lorem ipsum dolor sit amet.' 
+  },
+  
+  {
+    title:'group'      , 
+    contents:'Lorem ipsum dolor sit amet consectetur adipisicing elit.' ,
+    bgImg:'../multi/img/juice.jpg',
+    bgNarr:'juice 이미지에 대한 간략한 설명'
+  }
 ];
-//----------------------------------------------------------
+// --------------------------------------
+// 시나리오: 
+// .card 내부에 li요소 삽입
+// 별도의 data 객체를 만들어 해당하는 내용이 li에 첨부되게 진행
 
-/* 시나리오
-
-.card 내부에 li요소 삽입
-별도의 data객체를 만을어 해당하는 내용이 li에 첨부되게 진행
-
-*/
-
+// 변수 기존내용 선택
 var conBox = document.querySelector('#conBox');
 var cardUl = conBox.querySelector('.card');
 
-var i = 0 ;
-var len = data.length; //배열의 갯수만큼
-var makeLi; //li를 생성하여 담기위한 변수
-//============================================
-/* 점점 복잡해져가는 내용에 의해 추가작업이 어려움.
-for(; i<2; i+=1){
-  makeLi = document.createElement('Li'); 
-  //makeLi.innerText = data[i].title + ' '+ (i+1);
-  makeLi.innerHTML = '<div class="bg_img" style="background-image:url('+ data[i].bgImg+')" ><span class="blind">' + data[i].bgNarr + '</span></div><dl><dt>' + data[i].title + '</dt> <dd><span>' + data[i].contents +'</span><br /><a href=\" target=\"_blank\" ' + data[i].link + '\">자세히보기</a> </dd> </dl>';  //문자와 변수기리+로 이어주고 '',""로 문자 표시
-  cardUl.append(makeLi);//innerHTML은 중복을 허용하지 않고 내부자료를 삭제후 덮어 씌우기.. append: 기존자료에 추가
-}//escape 문자 =>\로 사용 :해당하는 글자가 어떠한 기능을 처리하게 만듬, 문자를 기능으로 기능을 문자로 
-*/ 
+var i = 0; 
+var len = data.length;
+var makeLi; 
+// ---------------------------------------
+// 기능 수행
+/* // 점점 복잡해져가는 내용에 의해 추가 작업이 어려움
+for(; i < 3; i+=1){
+  makeLi = document.createElement('li');
+  // makeLi.innerHTML = (i + 1) + data[i].title + ' ' + '<br />' + data[i].contents + ' ';
+  makeLi.innerHTML = '<div class="bg_img" style="background-image:url(' + (data[i].bgImg) + ')"><span class="blind">' + data[i].bgNarr + '</span></div><dl><dt>' + data[i].title + '</dt><dd><span>' + data[i].contents + '</span><br /><a href=\"' + data[i].link + '\" target=\"_blank\">자세히 보기</a></dd></dl>';
+  cardUl.appendChild(makeLi);
+}
+*/
 
-// ''와 "" 는  상황과 문맥에 따라 쓰임이 달라진다.
-//============================================
-// 우선기본 코드를 모두 담은 후에 필요한 부분만 색출해서 수정/첨부/삭제
+// 우선 기본 코드를 모두 담은 후에 필요한 부분만 색출해서 수정/첨부/삭제
+var baseCode = '<div class="bg_img"><span class="blind"></span></div>\
+                <dl>\
+                  <dt></dt>\
+                  <dd>\
+                    <span></span>\
+                    <br />\
+                    <a target="_blank">자세히 보기</a>\
+                  </dd>\
+                </dl>';
 
-var baseCode= '<div class="bg_img"><span class="blind"></span></div>\
-<dl>\
-  <dt></dt>\
-  <dd>\
-    <span></span>\
-    <br />\
-    <a target="_blank">자세히 보기</a>\
-  </dd>\
-</dl>';
-
-for(; i < len ; i +=1) {
+for(; i < len; i += 1){
   makeLi = document.createElement('li');
   makeLi.innerHTML = baseCode;
   cardUl.appendChild(makeLi);
 }
 
-//내용중 1번째 내용만 우선 정리해서 삽입
+// ======================================================
+// 내용 중 1번째 내용만 우선 정리해서 삽입
+// data[0].
+// bgImg: background-image , bgNarr: 설명
+// title : 제목
+// contents: 내용, link: link주소
+// ======================================================
+// 시나리오 2 : 
+// 1. li요소 첫번째 선택
+// 2. 공통 n번째를 가르키는 변수를 생성
+// ======================================================
 
-//data[0].
-//bgImg:background-image, bgNarr:설명
-//title :제목
-//content:내용, link:link주소
-//===============================================
-//시나리오2 :
-//1. li요소 첫번째 선택
-//2.공통 n번째를 가르키는 변수를 생성
-//===============================================
-var li = cardUl.querySelectorAll('li');
+var li         = cardUl.querySelectorAll('li');
 var setCardFn = function(n){
-  var idx=n;
-  var dt = li[idx].querySelector('dt');
-  var narr = li[idx].querySelector('dd>span');
-  var link = li[idx].querySelector('dd a');
-  var bgImg = li[idx].querySelector('.bg_img');
-  var bgNarr = bgImg.querySelector('span');
+  var idx        = n;
+  var dt         = li[idx].querySelector('dt');
+  var narr       = li[idx].querySelector('dd span');
+  var link       = li[idx].querySelector('dd a');
+  var bgImg      = li[idx].querySelector('.bg_img');
+  var bgNarr     = bgImg.querySelector('span');
   
-  //사용이미지가 없거나, 링크주소가 없거나 한다면() 해당요소는 삭제
-
-  dt.innerText = data[idx].title;
+  // 사용이미지없거나, 링크 주소가 없거나(!!undefined) 한다면 해당 요소는 삭제
+  dt.innerText   = data[idx].title;
   narr.innerText = data[idx].contents;
-  // link.href = data[idx].lin //html상에 존재하는 프로퍼티에 값을 처리
-  //data-...속성:사용자 속성 (사용자가 임의로 설정하는 속성)
-  //link.setAttribute('href', data[idx].lin); //어떠한 속성도 처리가능 data-속성
-  //<a data-href=""></a>
 
-  if(data[idx].bgImg !== undefined) {
+  ( !!data[idx].link ) ? link.href = data[idx].link : link.remove();
 
-    bgImg.style.backgroundImage = 'url('+data[idx].bgImg+')';
+  if(data[idx].bgImg !== undefined){ 
+    bgImg.style.backgroundImage = 'url(' + data[idx].bgImg + ')';
     bgNarr.innerText = data[idx].bgNarr;
-  }else {
-    bgImg.remove();
-  }   
-};// setCardFn()
-for ( i = 0 ; i < len; i +=1){
+  }else{
+    bgImg.remove();    
+  }
+
+}; // setCardFn(n)
+
+// li 내용 처리
+for(i = 0 ; i < len; i += 1){
   setCardFn(i);
 }
+
+// ---------------------------------------
+// cardUl.appendChild(makeLi); 여기서 innerHTML :(중복을 허용하지 않고 내부자료를 삭제 후 덮어 씌우기) 대신 append : 기존 자료에 추가 
+// escape 문자 : 해당하는 글자가 어떠한 기능을 처리하게 만드는 것을 \' \' \& \| \! , \n \t
+//link.setAttribute('href', data[idx].link); // 어떠한 속성도 처리 가능 data-속성
+
