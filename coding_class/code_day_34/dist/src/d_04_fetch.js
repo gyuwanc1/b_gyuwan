735c5d13611df7e4a8865c9ebccd5f5dfd589d11
gyuwanc <gyuwanc@gmail.com>
2021-12-09 16:32:32 +0900

    [modified] js fetch source

--- coding_class/code_day_34/dist/src/d_04_fetch.js
+++ coding_class/code_day_34/dist/src/d_04_fetch.js
@@ -1,60 +1,90 @@
 //d_04_fetch.js
 
-//기존 data불러오기 : XMLHttpRequest()
+
+/*
+  // 기존 data 불러오기 : XMLHttpRequest()
+  // http.open(method, url, async);
+  // 버튼 클릭시 관련 자료만 모아서 pre요소에 담기
+
+  // 변수 : 
+  const http = new XMLHttpRequest();
+  const elBtn = document.querySelector('button');
+  const elPre = document.querySelector('pre');
+
+  // 기능/함수
+  const dataList = function(){
+    http.open('GET', '../data/person_card.json', true);
+    http.onreadystatechange = function(){
+      const state = http.readyState;
+      if( state === XMLHttpRequest.DONE ){ // 요청이 끝나면
+        const sta = http.status;
+        if( sta >=200 && sta < 400 ){ // 200~399 -> 데이터 전송이 성공하면
+          // console.log( http.response);
+          // return http.response;
+          elPre.innerText = http.response;
+        }
+      }
+    }
+    http.send();
+  };
 
 
+  // 이벤트 :
+  elBtn.addEventListener('click', function(e){
+    e.preventDefault();
+    dataList();
+    // console.log( getData );
+  });
+
+*/
 
 
-// http.open ('GET','../data/pserson_card.json',true);
-// http.send();
-//버튼 클릭시 관련 자료만 모아서 pre요소에 담기
-
-// //변수:
-// const http = new XMLHttpRequest();
-// const elBtn  =  document.querySelector('button');
-// const elPre = document.querySelector('pre');
-
-// //기능 함수
-// const dataList = function(){
-//   http.open('GET','../data/person_card.json',true);
-//   http.onreadystatechange = function(){
-//    const state =  http.readyState;
-//    if(state === XMLHttpRequest.DONE){
-//      const sta = http.status;
-//      if( sta >=200 && sta < 400){
-//        //console.log(http.responseText);
-//       //  return http.response;
-//       elPre.innerText = http.response;
-//      }
-//     }
-
-//   }
-//   http.send();
-// }
-
-// //이벤트
-// // elBtn.addEventListener('click', function(e){
-// //   e.preventDefault();
-// //   dataList();
-//   //console.log( getData);
-// })
-
-//----------------------------------------------------------
-const elBtn  =  document.querySelector('button');
-const elPre = document.querySelector('pre');
+// ----------------------------------------------------
+const elBtn = document.querySelector('button');
+const elPre = document.querySelector('ul');
 let url = "../data/person_card.json";
 
-const fnFetch = function(path){
+class dataInsert {
+  constructor (title, image){
+    this.cardTitle = title;
+    this.imageUrl = image;
+  }
+}
 
-  fetch(path).then(function(response){
-    return response.json();
-  })  .then(function(data){
-      // elPre.innerText =JSON.stringify(data)
-    });
+const fnGetData = jsonData => {
+  const data = jsonData;
+  data.forEach( data => {
+    let objD = new dataInsert(data.title, data.image);
+    const mkLi = document.createElement('li');
+    mkLi.innerText = objD.cardTitle;
+    elPre.append(mkLi);    
+  });
+}
+
+// const fnFetch = function(path){
+//   fetch(path).then(function (response) {
+//     return response.json();
+//   }).then(function (data) {
+//     // elPre.innerText = JSON.stringify(data);
+//     fnGetData(data);
+//   });
+// };
+
+const fnFetch = path => {
+  fetch(path)
+  .then( response => response.json() )
+  .then( fnGetData );
 };
 
-elBtn.addEventListener('click', function(e){
+const fnEvent = e => {
   e.preventDefault();
-  fnFetch();
-})
+  fnFetch(url);
+};
 
+// 이벤트
+// elBtn.addEventListener('click', function(e){
+//   e.preventDefault();
+//   fnFetch(url);
+// });
+
+elBtn.addEventListener('click', fnEvent );
--- coding_class/code_day_34/js/src/d_04_fetch.js
+++ coding_class/code_day_34/js/src/d_04_fetch.js
@@ -1,49 +1,89 @@
 "use strict";
 
+function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
+
 //d_04_fetch.js
-//기존 data불러오기 : XMLHttpRequest()
-// http.open ('GET','../data/pserson_card.json',true);
-// http.send();
-//버튼 클릭시 관련 자료만 모아서 pre요소에 담기
-// //변수:
-// const http = new XMLHttpRequest();
-// const elBtn  =  document.querySelector('button');
-// const elPre = document.querySelector('pre');
-// //기능 함수
-// const dataList = function(){
-//   http.open('GET','../data/person_card.json',true);
-//   http.onreadystatechange = function(){
-//    const state =  http.readyState;
-//    if(state === XMLHttpRequest.DONE){
-//      const sta = http.status;
-//      if( sta >=200 && sta < 400){
-//        //console.log(http.responseText);
-//       //  return http.response;
-//       elPre.innerText = http.response;
-//      }
-//     }
-//   }
-//   http.send();
-// }
-// //이벤트
-// // elBtn.addEventListener('click', function(e){
-// //   e.preventDefault();
-// //   dataList();
-//   //console.log( getData);
-// })
-//----------------------------------------------------------
+
+/*
+  // 기존 data 불러오기 : XMLHttpRequest()
+  // http.open(method, url, async);
+  // 버튼 클릭시 관련 자료만 모아서 pre요소에 담기
+
+  // 변수 : 
+  const http = new XMLHttpRequest();
+  const elBtn = document.querySelector('button');
+  const elPre = document.querySelector('pre');
+
+  // 기능/함수
+  const dataList = function(){
+    http.open('GET', '../data/person_card.json', true);
+    http.onreadystatechange = function(){
+      const state = http.readyState;
+      if( state === XMLHttpRequest.DONE ){ // 요청이 끝나면
+        const sta = http.status;
+        if( sta >=200 && sta < 400 ){ // 200~399 -> 데이터 전송이 성공하면
+          // console.log( http.response);
+          // return http.response;
+          elPre.innerText = http.response;
+        }
+      }
+    }
+    http.send();
+  };
+
+
+  // 이벤트 :
+  elBtn.addEventListener('click', function(e){
+    e.preventDefault();
+    dataList();
+    // console.log( getData );
+  });
+
+*/
+// ----------------------------------------------------
 var elBtn = document.querySelector('button');
-var elPre = document.querySelector('pre');
+var elPre = document.querySelector('ul');
 var url = "../data/person_card.json";
+
+var dataInsert = function dataInsert(title, image) {
+  _classCallCheck(this, dataInsert);
+
+  this.cardTitle = title;
+  this.imageUrl = image;
+};
+
+var fnGetData = function fnGetData(jsonData) {
+  var data = jsonData;
+  data.forEach(function (data) {
+    var objD = new dataInsert(data.title, data.image);
+    var mkLi = document.createElement('li');
+    mkLi.innerText = objD.cardTitle;
+    elPre.append(mkLi);
+  });
+}; // const fnFetch = function(path){
+//   fetch(path).then(function (response) {
+//     return response.json();
+//   }).then(function (data) {
+//     // elPre.innerText = JSON.stringify(data);
+//     fnGetData(data);
+//   });
+// };
+
 
 var fnFetch = function fnFetch(path) {
   fetch(path).then(function (response) {
     return response.json();
-  }).then(function (data) {// elPre.innerText =JSON.stringify(data)
-  });
+  }).then(fnGetData);
 };
 
-elBtn.addEventListener('click', function (e) {
+var fnEvent = function fnEvent(e) {
   e.preventDefault();
-  fnFetch();
-});
+  fnFetch(url);
+}; // 이벤트
+// elBtn.addEventListener('click', function(e){
+//   e.preventDefault();
+//   fnFetch(url);
+// });
+
+
+elBtn.addEventListener('click', fnEvent);
