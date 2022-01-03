e7186dc0e11a155ea9c3d88bc9e665fa05e9ee5a
b_gyuwan <gyuwanc@gmail.com>
2022-01-03 15:22:27 +0900

    [add] react method_02

--- coding_class/coding_class_react/src/Component/Header.js
+++ coding_class/coding_class_react/src/Component/Header.js
@@ -1,4 +1,7 @@
 // Header.js
+
+import Gnb from "./Gnb";
+
 export default function Header({site, url}){
   // const props = {  site:'xido',  url : 'http://naver.com' }
   // const myName = 'xido-school';
@@ -6,25 +9,19 @@ export default function Header({site, url}){
   // const url = "http://naver.com";
 
   const h1Style = { color:'#05f', textTransform: 'uppercase' };
-  const navLists = ['about','product','gallery','notice'];
+  
 
   return (
       <header id="headBox">
         <h1 style={h1Style}>
           <a href={url} target="_blank">{site}</a>
           </h1>
-          <nav className="gnb">
-            <h2>navigation</h2>
-            <ul>
-              {/* <li><a href="/about">about</a></li>
-              <li><a href="/product">product</a></li>
-              <li><a href="/gallery">gallery</a></li>
-              <li><a href="/notice">notice</a></li> */}
-              { navLists.map( (li, idx) => <li key={idx}>{li}</li> ) }
-            </ul>
-          </nav>
+          <Gnb />
       </header>
   )
+}
+Header.defaultProps = {
+  site :'gyuwanc'
 }
 
 
