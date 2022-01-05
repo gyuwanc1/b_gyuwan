4135b5e3f1abbb05d69967cc19de95e585dc1042
b_gyuwan <gyuwanc@gmail.com>
2022-01-05 17:12:16 +0900

    [add] react useEffect

--- coding_class/coding_class_react/src/App.js
+++ coding_class/coding_class_react/src/App.js
@@ -7,30 +7,37 @@ import './App.css';
 function App() {
   const title = 'site';
 
-  const style={backgroundColor:"#add",
-  color:"#fff",
-  padding:"1rem",
-  border:"1px solid #333"
-}
-// let  par = '변경전';
-const dataArr = [
-  '내용변경전','값이 변경되었습니다.'
-]
-const [par, setPar] = useState(0);
-const pCng = (e)=>{
-  e.preventDefault();  
-  setPar(par+1)
-}
+//   const style={backgroundColor:"#add",
+//   color:"#fff",
+//   padding:"1rem",
+//   border:"1px solid #333"
+// }
+// // let  par = '변경전';
+// const dataArr = [
+//   '내용변경전','값이 변경되었습니다.'
+// ]
+// const [par, setPar] = useState(0);
+// const pCng = (e)=>{
+//   e.preventDefault();  
+//   setPar(par+1)
+// }
+
+//함수형 리액트 문법
+//1. 컴포넌트
+//2. property명 : class => className, for => htmlFor
+//3. js기능을 작성하는 위치 - return부분을 제외한 영역 ,jsx에서  { } 영역에서 사용
+//4. 이벤트 핸들러 onClick, onChange, onFocus
+//5. react Hook : 필요한 기능을 요구시 뽑아오는 것 
+// 5 -1. useState: 변수의 값이 변경될때 그것을 재처리하기위한 기능
+// 5 -2. useEffect: 변화할때마다 그변화를 체크, 필요데이터를 불러와서 확인 
 
   return (
     <div className="App">
-      <button style={style} onClick={pCng}>클릭</button>
-      <p>{par}</p>
-
-
-      {/* <Header heading={title} />
+      {/* <button style={style} onClick={pCng}>클릭</button>
+      <p>{par}</p> */}
+      <Header heading={title} />
       <Main />    
-      <Footer heading={title} /> */}
+      <Footer heading={title} />
     </div>
   );
 }
