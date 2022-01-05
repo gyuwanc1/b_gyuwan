04c9b707dfc20ac6d48a2e28cfa407be7044bde9
b_gyuwan <gyuwanc@gmail.com>
2022-01-05 15:31:49 +0900

    [add] useEffect

--- coding_class/coding_class_react/src/App.js
+++ coding_class/coding_class_react/src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Header from './component/Header';
 import Main from './component/Main';
 import Footer from './component/Footer';
@@ -7,13 +7,34 @@ import './App.css';
 function App() {
   const title = 'site';
 
+  const style={backgroundColor:"#add",
+  color:"#fff",
+  padding:"1rem",
+  border:"1px solid #333"
+}
+// let  par = '변경전';
+const dataArr = [
+  '내용변경전','값이 변경되었습니다.'
+]
+const [par, setPar] = useState(0);
+const pCng = (e)=>{
+  e.preventDefault();  
+  setPar(par+1)
+}
+
   return (
     <div className="App">
-      <Header heading={title} />
+      <button style={style} onClick={pCng}>클릭</button>
+      <p>{par}</p>
+
+
+      {/* <Header heading={title} />
       <Main />    
-      <Footer heading={title} />
+      <Footer heading={title} /> */}
     </div>
   );
 }
+
+
 
 export default App;
